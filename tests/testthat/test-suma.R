3a738d1bf2b3a0ed5b0dafff6875ce8405ee0bee
bautitorre12 <bautistatorregiani@gmail.com>
2024-10-31 19:14:02 -0300

    agrego tests para mejorar el porcentaje de eficacia

--- tests/testthat/test-suma.R
+++ tests/testthat/test-suma.R
@@ -6,3 +6,11 @@ test_that("la suma funciona", {
 test_that("la suma no funciona para valores no numericos", {
   expect_error(suma(2,'2', "Los argumentos deben ser numericos"))
 })
+
+test_that("suma muestra un error si 'x' es negativo", {
+  expect_error(suma(-5, 3), "No puedo sumar negativos: el argumento 'x' es negativo")
+})
+
+test_that("suma muestra un error si 'y' es negativo", {
+  expect_error(suma(3, -5), "No puedo sumar negativos: el argumento 'y' es negativo")
+})
